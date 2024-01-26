0520fac34627145ae2f2c5f7d5f7e5b52b72951f
Sumit Kamra <sumitkamra@gmail.com>
2024-01-26 23:48:08 +0700

    Package blueprint

---
+++ create_structure.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir notebooks melanoma_classification melanoma_classification/interface melanoma_classification/ml_logic tests
+touch Makefile
+touch requirements.txt
+touch setup.py
+
+touch melanoma_classification/__init__.py
+
+touch melanoma_classification/interface/__init__.py
+touch melanoma_classification/interface/main_local.py
+
+touch melanoma_classification/ml_logic/__init__.py
+touch melanoma_classification/ml_logic/data.py
+touch melanoma_classification/ml_logic/model.py
+touch melanoma_classification/ml_logic/preprocessor.py
+touch melanoma_classification/ml_logic/registry.py
+
+touch melanoma_classification/utils.py
+touch melanoma_classification/params.py
+touch .gitignore
+
+touch tests/__init__.py
